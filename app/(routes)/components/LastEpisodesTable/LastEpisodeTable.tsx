80410071bb8d70fb574ab43c32719ea2babd5992
Maicol Argumedo <margumedo.sm@gmail.com>
2024-10-16 13:47:13 -0400

    [add]: list-episodes inital

---
+++ app/(routes)/components/LastEpisodesTable/LastEpisodeTable.tsx
@@ -0,0 +1,164 @@
+"use client";
+
+import * as React from "react";
+import {
+    ColumnDef,
+    ColumnFiltersState,
+    PaginationState,
+    SortingState,
+    VisibilityState,
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    useReactTable,
+} from "@tanstack/react-table";
+
+import { Button } from "@/components/ui/button";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+
+import { useEffect } from "react";
+import { lastEpisodesTableTypes } from "./LastEpisodesTable.types";
+import { useEpisodeStore } from "@/store/episodesStore";
+
+
+export const columns: ColumnDef<lastEpisodesTableTypes>[] = [
+    {
+        accessorKey: "id",
+        header: "ID",
+        cell: ({ row }) => <div>{row.getValue("id")}</div>,
+    },
+    {
+        accessorKey: "name",
+        header: "Name",
+        cell: ({ row }) => <div>{row.getValue("name")}</div>,
+    },
+    {
+        accessorKey: "air_date",
+        header: "Air Date",
+        cell: ({ row }) => <div>{row.getValue("air_date")}</div>,
+    },
+    {
+        accessorKey: "episode",
+        header: "Episode",
+        cell: ({ row }) => <div>{row.getValue("episode")}</div>,
+    },
+
+];
+
+
+export function LastEpisodeTable() {
+
+    const { episodes, fetchEpisodes } = useEpisodeStore();
+
+    const [sorting, setSorting] = React.useState<SortingState>([
+        {
+            id: "id",
+            desc: true
+        },
+    ]);
+
+    const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({});
+    const [rowSelection, setRowSelection] = React.useState({});
+    const [pagination, setPagination] = React.useState<PaginationState>({
+        pageIndex: 0,
+        pageSize: 8,
+    });
+    const table = useReactTable({
+        data: episodes,
+        columns,
+        pageCount: Math.ceil(episodes.length / pagination.pageSize),
+        onPaginationChange: setPagination,
+        onSortingChange: setSorting,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        onColumnVisibilityChange: setColumnVisibility,
+        onRowSelectionChange: setRowSelection,
+        state: {
+            sorting,
+            columnVisibility,
+            rowSelection,
+            pagination,
+        },
+    });
+
+    useEffect(() => {
+        fetchEpisodes();
+    }, [fetchEpisodes]);
+
+    return (
+        <div className="w-full">
+            <div className="rounded-md border">
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead key={header.id}>
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(header.column.columnDef.header, header.getContext())}
+                                        </TableHead>
+                                    );
+                                })}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell key={cell.id}>
+                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={columns.length} className="h-24 text-center">
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+            <div className="flex items-center justify-end space-x-2 py-4">
+                <div className="space-x-2">
+                    <Button
+                        variant="outline"
+                        size="sm"
+                        onClick={() => table.previousPage()}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        Previous
+                    </Button>
+                    <Button
+                        variant="outline"
+                        size="sm"
+                        onClick={() => table.nextPage()}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        Next
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
